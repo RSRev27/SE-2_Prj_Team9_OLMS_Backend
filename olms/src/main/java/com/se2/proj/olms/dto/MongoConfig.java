0369e67e5a1f7afcfd4c6f4a1b6b3327a7815dbd
Venkata Ravi Teja Sandram <87124365+RSRev27@users.noreply.github.com>
2024-11-07 09:37:32 +0000

    Modified MongoConfig file

--- olms/src/main/java/com/se2/proj/olms/dto/MongoConfig.java
+++ olms/src/main/java/com/se2/proj/olms/dto/MongoConfig.java
@@ -1,8 +1,6 @@
 package com.se2.proj.olms.dto;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
